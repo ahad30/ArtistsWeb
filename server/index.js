87cb9ff4450256f5e16ae4d14a7e4a2636a8bc3e
ahad30 <mohiminulislamahad@gmail.com>
2025-01-10 22:03:27 +0600

    asds

--- server/index.js
+++ server/index.js
@@ -27,7 +27,10 @@ app.use(cors(corsOptions));
 app.use(express.json());
 
 // Connect to MongoDB
-mongoose.connect("mongodb://localhost:27017/admin").then(() => {
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => {
   console.log("Connected to MongoDB");
 }).catch((error) => {
   console.error("MongoDB connection error:", error);
